05a0976265cea571c56b87a764f45b06a3c57a69
559826926 <46727562+559826926@users.noreply.github.com>
2020-03-06 14:29:43 -0600

    Create Song.java

---
+++ Song.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Song{
+
+    Scanner scan = new Scanner(db);
+    File db = new File("db.txt");
+    ArrayList<MeasureInfo> measures = new ArrayLIst<MeasureInfo>();
+    public Song(){
+        while (scan.hasNextLine()){
+            
+
+        }
+
+    }
+
+
+
+
+
+}
