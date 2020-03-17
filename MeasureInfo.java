81deb4babc440162fecd62867a54b3c462a4ffb4
GrantLarsen314 <54328249+GrantLarsen314@users.noreply.github.com>
2020-03-16 20:01:12 -0500

    Update MeasureInfo.java
    Added an array that has the accented and non-accented notes in the array. This was added to the MeasureInfo object constructors. Also added a getter for the measure number so I could have the app print out the measure number at the start of a new measure.

--- MeasureInfo.java
+++ MeasureInfo.java
@@ -7,7 +7,7 @@ public class MeasureInfo{
   private int numOfBeats;
   private int noteType;
   private String timeSignature;
-  
+  private boolean[] accentedNote;
   
   
   //Basic MeasureInfo constructor method
@@ -17,24 +17,39 @@ public class MeasureInfo{
     numOfBeats = 3;
     noteType = 4;
     timeSignature = "3/4";
+    accentedNote = new boolean[3];
   //We will need to find a way to parse the input time signiture into its two ints: the number of beats and what beat gets the count.
   //For a minimum viable produce, we can probably ignore the "noteType" until we get the rest of the metronome under control
   }
   //MeasureInfo constructor requiring inputs (probably going to use this a lot more)
-  public MeasureInfo(int mn, int mbpm, int nob, int nt, String ts){
+  public MeasureInfo(int mn, int mbpm, int nob, int nt, String ts, boolean[] an){
     measureNum = mn;
     measurebpm = mbpm;
     numOfBeats = nob;
     noteType = nt;
     timeSignature = ts;
+    accentedNote = an;
   }
   
+  //Gets an assortment of measure stuff
   public int[] getMeasureStuff(){
     int[] measureStuff = new int[2];
     measureStuff[0] = this.numOfBeats;
     measureStuff[1] = this.measurebpm;
     return measureStuff;
 
+  }
+  
+  //Getters and setters for the accentedNotes array
+  //The list works by having the length be equal to the number of beats in the measure
+  //and the true values are for accented beats and false is for unaccented
+  public boolean getAccentedNoteBoolean(int i){
+      return accentedNote[i];
+  }
+  
+  //Gets measure number
+  public int getMeasureNum(){
+      return measureNum;
   }
   
   //Might not use this much but ya neva no
